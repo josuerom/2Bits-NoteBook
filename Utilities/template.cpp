2a0bcdf5e9090e903d5a3635fa358f6d4c61392c
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-11-26 16:45:16 -0500

    NoteBook Team 2 Bits()

---
+++ Utilities/template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define se second
+#define fi first
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef unsigned long long ull;
+
+const int N = 1e6 + 20;
+
+int main () {
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+#ifdef DEBUG
+   freopen("in1", "r", stdin);
+#else
+   #define endl '\n'
+#endif
+   // Solution
+   return 0; 
+}
