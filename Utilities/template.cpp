90302bbbf93feb61ab2c05c1e9e0a166eeb35d02
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-12-01 21:48:17 -0500

    Update template.cpp

--- Utilities/template.cpp
+++ Utilities/template.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 #define pb push_back
 #define mp make_pair
-#define se second
-#define fi first
+#define ss second
+#define ff first
 
 typedef long long ll;
 typedef pair<int,int> pii;
 typedef unsigned long long ull;
 
 const int N = 1e6 + 20;
+int a[N], n;
 
 int main () {
    ios::sync_with_stdio(0);
    cin.tie(0);
-#ifdef DEBUG
+#ifdef LOCAL
    freopen("in1", "r", stdin);
 #else
    #define endl '\n'
